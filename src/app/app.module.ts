02c97add367cf1c8dd4ad08c93d69fdcce2ff848
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-01-07 14:05:55 +0300

    Kullanıcıdan data alıp , listeleme işlemi yapıldı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { DetailComponent } from './detail/detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,9 +9,36 @@ export class ProductComponent implements OnInit {
 
   pageSlogan="Product Slogan";
 
+  title=""
+  detail=""
+  //productArr=[{title:"",detail:""}]
+  productArr=[]
+  count=1
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fncInsertProduct(){
+
+   //console.log(this.title+" " + this.detail)
+
+   if(this.title===""||this.detail===""){
+     alert("Fail")
+   }
+   else{
+
+    const item= {count:this.count,title:this.title,detail:this.detail}
+
+    this.productArr.push(item)
+
+    console.log(JSON.stringify(this.productArr))
+
+    this.title=""
+    this.detail=""
+    this.count++;
+   }
+
   }
 
 }
