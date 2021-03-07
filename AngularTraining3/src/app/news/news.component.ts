061fc593fdb47f6a85e76df59a401ab7b2ed28ed
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-07 17:19:55 +0300

    Model oluşturuldu.

--- AngularTraining3/src/app/news/news.component.ts
+++ AngularTraining3/src/app/news/news.component.ts
@@ -1,8 +1,9 @@
-import { UserModel } from './../user-model';
-import { NewsModel } from './../news-model';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import *as $  from  'jquery'
+import { HttpClient } from '@angular/common/http'
+import { NewsModel, Article } from '../news-model'
+import { UserModel } from '../user-model'
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-news',
@@ -11,87 +12,92 @@ import *as $  from  'jquery'
 })
 export class NewsComponent implements OnInit {
 
-  jsonObj:NewsModel|undefined;
-  userArr: UserModel[] =[]
+  jsonObj: NewsModel | undefined;
+  newsArr: Article[] = [];
+  userArr: UserModel[] = [];
 
-  constructor(private http:HttpClient) {
-   const kubi:UserModel={
-     age:10,
-     id:5,
-     mail:"asdasd",
-     name:"ali"
-   }
-   // this.userArr?.push({id:10,name:"ali",age:25,mail:"asda@hotm.com"})
-   this.userArr.push(kubi)
-
-   }
+  constructor( private http: HttpClient) {
+    const ali1 :UserModel = {
+      age: 36,
+      id: 10,
+      mail: "ali@gmail",
+      name : "ali"
+    }
+    this.userArr.push(ali1)
+    this.userArr.push(ali1)
+    this.userArr.push(ali1)
+  }
 
   ngOnInit(): void {
 
 
+
     const params = {
-      source :"",
-      apiKey:""
+      sources: "google-news-in",
+      apiKey: "38a9e086f10b445faabb4461c4aa71f8"
     }
-    const url =""
+    const url = "http://newsapi.org/v2/top-headlines";
 
-    this.http.get(url,{params:params}).toPromise().then(response => {
+    this.http.get(url, { params: params })
+    .toPromise()
+    .then(response => {
 
-      this.jsonObj=response as NewsModel
+      this.jsonObj = response as NewsModel;
+      console.log(this.jsonObj)
+      this.newsArr = this.jsonObj.articles
 
-    }).catch(err=>{
-      console.log(err+" " +err.message);
+    }).catch( err => {
+      console.log("Error : " + err.message)
     })
 
-   this.userArr.map((item, index)=>{
-    console.log(item.name)
-   })
 
-   this.fncExecute1().then(()=>{
+    this.userArr.map( (item, index) => {
+      console.log(item.name)
+    } )
 
-     this.fncExecute2().then (()=>{
-
-      this.fncExecute3().then (()=>{
-
-        console.log("tamam")
+    this.fncExecute1().then( () => {
+      this.fncExecute2().then( () => {
+        this.fncExecute3().then( () => {
+          console.log("All fnc Complete")
+        })
       })
+    })
 
-     })
-
-   })
+    console.log("All Time Call")
 
 
-  // this.fncExecute2()
-   //this.fncExecute3()
+    $(document).ready( () => {
+      $("div#card").animate({right: 0});
+    })
 
   }
 
-  fncExecute1(){
-    return new Promise <void>((resolve,reject)=>{
+
+
+  fncExecute1() {
+    return new Promise<void>((resolve, reject) => {
       console.log("fncExecute1")
-       resolve()
-    })
-
-  }
-  fncExecute2(){
-    return new Promise<void>((resolve,reject)=>{
-
-      setTimeout(()=>{
-        console.log("fncExecute2")
-        resolve()
-      },2000)
-
-    })
-
-
-  }
-  fncExecute3(){
-    return new Promise<void>((resolve,reject)=>{
-
-      console.log("fncExecute3")
       resolve()
     })
 
+  }
+
+  fncExecute2() {
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        console.log("fncExecute2")
+        resolve()
+      }, 4000);
+    })
+
+
+  }
+
+  fncExecute3() {
+    return new Promise<void>((resolve, reject) => {
+      console.log("fncExecute3")
+      resolve()
+    })
   }
 
 
